110f84bc89ef5d7563c634b67a258dea5fee12b9
allan <allan.riberio@gmail.com>
2022-01-28 11:59:16 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,24 @@
+//Crie um array que receba 5 itens e exiba no console.
+let esportes = ["futebol", "volei", "basquete", "rugby", "hoquei"]
+console.log (esportes)
+
+//Utilize um método para adicionar um nome ao inicio do array.
+esportes.unshift("críquete")
+console.log (esportes)
+
+//Utilize um método para remover o último nome do array.
+esportes.pop()
+console.log(esportes)
+
+//Utilize um método para adicionar dois nomes ao fim do array.
+esportes.push("neymar","messi")
+console.log(esportes)
+
+//Utilize um método para remover o primeiro nome do array.
+esportes.shift()
+console.log (esportes)
+
+//Utilize um método para organizar em ordem crescente o seguinte array: const numbers = [7,5,6,3,8,9,2,1,4]
+const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
